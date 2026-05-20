d6542051c58cdc2b173aa1f422417fdcd2b8e57f
doyounko1231 <doyounko1231@gmail.com>
2026-05-21 02:13:22 +0900

    feature: Blog 생성, 조회, 수정, 삭제 Service 구현

---
+++ src/main/kotlin/kr/app/calto/service/impl/BlogServiceImpl.kt
@@ -0,0 +1,73 @@
+package kr.app.calto.service.impl
+
+import kr.app.calto.controller.dto.request.blog.CreateBlogRequest
+import kr.app.calto.controller.dto.request.blog.UpdateBlogRequest
+import kr.app.calto.domain.BlogColor
+import kr.app.calto.infrastructure.entities.BlogEntity
+import kr.app.calto.infrastructure.repository.BlogRepository
+import kr.app.calto.service.BlogService
+import kr.app.calto.service.dto.BlogDetail
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+@Service
+class BlogServiceImpl(
+    private val blogRepository: BlogRepository,
+) : BlogService {
+    override fun getAllBlogs(): List<BlogDetail> {
+        val blogs =
+            blogRepository
+                .findAll()
+                .map { BlogDetail.from(it.toDomain()) }
+
+        return blogs
+    }
+
+    override fun getBlogById(id: Long): BlogDetail {
+        val blog =
+            blogRepository
+                .findById(id)
+                .map { BlogDetail.from(it.toDomain()) }
+                .orElseThrow { NoSuchElementException("블로그 조회 실패") }
+
+        return blog
+    }
+
+    override fun createBlog(createBlogRequest: CreateBlogRequest) {
+        val entity =
+            BlogEntity(
+                name = createBlogRequest.name,
+                members = 1,
+                imageUrl = createBlogRequest.imageUrl ?: "default-image.jpg",
+                mainColor = BlogColor.WHITE,
+            )
+        blogRepository.save(entity)
+    }
+
+    override fun updateBlog(
+        blogId: Long,
+        updateBlogRequest: UpdateBlogRequest,
+    ) {
+        val entity =
+            blogRepository
+                .findById(blogId)
+                .orElseThrow { NoSuchElementException("블로그 조회 실패") }
+
+        updateBlogRequest.name?.let { entity.name = it }
+        updateBlogRequest.imageUrl?.let { entity.imageUrl = it }
+        updateBlogRequest.mainColor?.let { entity.mainColor = it }
+        entity.updatedAt = LocalDateTime.now()
+
+        blogRepository.save(entity)
+    }
+
+    override fun deleteBlog(blogId: Long) {
+        val entity =
+            blogRepository
+                .findById(blogId)
+                .orElseThrow { NoSuchElementException("블로그 조회 실패") }
+
+        entity.deletedAt = LocalDateTime.now()
+        blogRepository.save(entity)
+    }
+}
