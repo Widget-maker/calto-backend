d000565a39ee68c1b957a7922d42711c910b81d3
doyounko1231 <doyounko1231@gmail.com>
2026-05-21 01:58:01 +0900

    feature: Blog, BlogMember Repository 생성

---
+++ src/main/kotlin/kr/app/calto/infrastructure/repository/BlogMemberRepository.kt
@@ -0,0 +1,13 @@
+package kr.app.calto.infrastructure.repository
+
+import kr.app.calto.infrastructure.entities.BlogMemberEntity
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface BlogMemberRepository : JpaRepository<BlogMemberEntity, Long> {
+    fun findByBlogIdAndId(
+        blogId: Long,
+        id: Long,
+    ): BlogMemberEntity?
+
+    fun findByBlogId(blogId: Long): List<BlogMemberEntity>
+}
