65408967982cc9fda14182d6284437e2a77367bc
doyounko1231 <doyounko1231@gmail.com>
2026-06-24 20:43:34 +0900

    refactor: expiresAt 포맷팅을 서비스 단계로 이동

--- src/main/kotlin/kr/app/calto/service/dto/InviteCreatedResult.kt
+++ src/main/kotlin/kr/app/calto/service/dto/InviteCreatedResult.kt
@@ -1,8 +1,6 @@
 package kr.app.calto.service.dto
 
-import java.time.LocalDateTime
-
 class InviteCreatedResult(
     val inviteUrl: String,
-    val expiresAt: LocalDateTime,
+    val expiresAt: String,
 )
