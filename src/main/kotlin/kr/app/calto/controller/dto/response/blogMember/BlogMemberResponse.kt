864a93e3e3e9fe5c6b3f3b9b54d84e006e0bcd5f
doyounko1231 <doyounko1231@gmail.com>
2026-06-27 23:33:35 +0900

    chore: BlogMemberResponse 분리 검토 TODO 주석 추가

--- src/main/kotlin/kr/app/calto/controller/dto/response/blogMember/BlogMemberResponse.kt
+++ src/main/kotlin/kr/app/calto/controller/dto/response/blogMember/BlogMemberResponse.kt
@@ -3,6 +3,7 @@ package kr.app.calto.controller.dto.response.blogMember
 import kr.app.calto.domain.MemberRole
 import kr.app.calto.service.dto.BlogMemberDetail
 
+// TODO: 단건 조회, list 조회, 본인 조회 response 분리에 대해 고려
 class BlogMemberResponse(
     val id: Long,
     val name: String,
